71c8f82a227779d738c7983bd34cd3ac8225d3ac
Jeremy Gold <jeremy.gold@ikegps.com>
2019-02-06 19:03:24 +1300

    Downloading processing to specified file

--- setup-goldilux.sh
+++ setup-goldilux.sh
@@ -5,7 +5,7 @@ sudo apt -y upgrade
 
 if [ ! -f ~/processing-3.5.3/bin/processing ]; then
   echo "Installing Processing"
-  wget http://download.processing.org/processing-3.5.3-linux-armv6hf.tgz
-  tar xvzf processing-3.5.3-linux-armv6hf.tgz
+  wget -O processing.tgz http://download.processing.org/processing-3.5.3-linux-armv6hf.tgz
+  tar xvzf processing.tgz
 fi
 
