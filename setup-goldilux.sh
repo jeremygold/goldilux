eb35bb11851446631ebe8db5cc4733855c589c9a
Jeremy Gold <jeremy.gold@ikegps.com>
2019-02-06 21:27:27 +1300

    Clone and build libusb and freenect

--- setup-goldilux.sh
+++ setup-goldilux.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-sudo apt update
-sudo apt -y upgrade
+# FIXME - bring these back for full script
+# sudo apt update
+# sudo apt -y upgrade
+# sudo apt -y autoremove
+
+################################################################################
+##### Download and extract processing #####
 
 processing_download=processing-3.5.3-linux-armv6hf.tgz
 
@@ -20,12 +25,39 @@ else
   echo "Processing already installed"
 fi
 
+
+################################################################################
+##### Clone, build, and install libusb #####
+
+sudo apt -y install autotools-dev autoconf libtool libudev-dev
 cd ~
-git clone https://github.com/OpenKinect/libfreenect.git
-cd libfreenect
-mkdir build
-cd build
-cmake -L .. # -L lists all the project options
+if [ ! -d libusb ]; then
+  git clone https://github.com/libusb/libusb.git
+else
+  echo "libusb already cloned"
+fi
+
+cd libusb
+./autogen.sh
 make && sudo make install
 
-# TODO: Download, build and install libfreenect
+################################################################################
+##### Clone, build, and install libfreenect #####
+
+sudo apt -y install cmake
+cd ~
+if [ ! -d libfreenect ]; then
+  git clone https://github.com/OpenKinect/libfreenect.git
+else
+  echo "libfreenect already cloned"
+fi
+
+cd libfreenect
+mkdir -p build
+cd build
+cmake -L -DLIBUSB_1_INCLUDE_DIR=/home/pi/libusb/libusb/ -DLIBUSB_1_LIBRARY=/usr/local/lib/libusb-1.0.so ..
+make && sudo make install
+
+wget https://github.com/OpenKinect/libfreenect/blob/master/platform/linux/udev/51-kinect.rules
+sudo mv 51-kinect.rules /etc/udev/rules.d/
+
