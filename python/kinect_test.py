2f37b665c124555598bf86fba0f50d34fba13a65
Jeremy Gold <jeremy.gold@ikegps.com>
2019-07-07 16:40:29 +1200

    Initial implementation of blob detector

--- python/kinect_test.py
+++ python/kinect_test.py
@@ -52,61 +52,62 @@ def show_depth(warp = False):
         color_scale = cv2.bitwise_and(color_img, scaled)
         image = cv2.bitwise_or(color_scale, depth_rgb)
 
-        overlay = cv2.bitwise_xor(rgb, image)
-        cv2.line(overlay, (0, y_threshold), (w, y_threshold), (0, 255, 0), 3)
-
-        params = cv2.SimpleBlobDetector_Params()
-        params.minThreshold = 0
-        params.maxThreshold = 255
-        params.minArea = 200
-        params.maxArea = w * h
-        params.filterByArea = True
-
-        params.filterByCircularity = False
-        params.filterByConvexity = False
-        params.filterByColor = False
-        params.filterByInertia = False
-
-        detector = cv2.SimpleBlobDetector(params)
-        keypoints = detector.detect(depth)
-        print("Detected " + str(len(keypoints)))
+        # overlay = cv2.bitwise_xor(rgb, image)
+        cv2.line(image, (0, y_threshold), (w, y_threshold), (0, 255, 0), 3)
 
         # overlay_with_keypoints = cv2.drawKeypoints(overlay, keypoints, np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-        for keypoint in keypoints:
-            cX = int(keypoint.pt[0])
-            cY = int(keypoint.pt[1])
-                
-            location = str(cX) + "," + str(cY)
-            cv2.circle(overlay, (cX, cY), 15, (255, 0, 255), -1)
-            cv2.putText(overlay, location, (cX-25, cY-25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+        if True:
+            params = cv2.SimpleBlobDetector_Params()
+            params.minThreshold = 0
+            params.maxThreshold = 255
+            params.minArea = 200
+            params.maxArea = w * h
+            params.filterByArea = True
 
-            if(last_note >= 0):
-                # Already playing a note
-                if(cY > y_threshold):
-                    # Center is below line, stop playing
-                    client.publish("note-off", last_note)
-                    last_note = -1
+            params.filterByCircularity = False
+            params.filterByConvexity = False
+            params.filterByColor = False
+            params.filterByInertia = False
+
+
+            detector = cv2.SimpleBlobDetector(params)
+            keypoints = detector.detect(depth)
+            print("Detected " + str(len(keypoints)))
+            for keypoint in keypoints:
+                cX = int(keypoint.pt[0])
+                cY = int(keypoint.pt[1])
+                    
+                location = str(cX) + "," + str(cY)
+                cv2.circle(image, (cX, cY), 15, (255, 0, 255), -1)
+                cv2.putText(image, location, (cX-25, cY-25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+
+                if(last_note >= 0):
+                    # Already playing a note
+                    if(cY > y_threshold):
+                        # Center is below line, stop playing
+                        client.publish("note-off", last_note)
+                        last_note = -1
+                    else:
+                        # Center is still above the line, leave current note
+                        client.publish("note-off", last_note)
+                        note = int(map_range((0, w), (88, 0), cX))
+                        # Only change if we've moved by 2 semitones
+                        if(abs(note - last_note) > 2):
+                            last_note = note
+                            client.publish("note-on", note)
+                        pass
                 else:
-                    # Center is still above the line, leave current note
-                    client.publish("note-off", last_note)
-                    note = int(map_range((0, w), (88, 0), cX))
-                    # Only change if we've moved by 2 semitones
-                    if(abs(note - last_note) > 2):
+                    # No active note
+                    if(cY < y_threshold):
+                        # Center is above the line, start a new note
+                        note = int(map_range((0, w), (88, 0), cX))
                         last_note = note
                         client.publish("note-on", note)
-                    pass
-            else:
-                # No active note
-                if(cY < y_threshold):
-                    # Center is above the line, start a new note
-                    note = int(map_range((0, w), (88, 0), cX))
-                    last_note = note
-                    client.publish("note-on", note)
 
-                else:
-                    # Center is below the line, don't start anything
-                    pass
+                    else:
+                        # Center is below the line, don't start anything
+                        pass
 
 
         if(warp):
@@ -128,7 +129,8 @@ def show_depth(warp = False):
 
             cv2.imshow('Depth', warped)
         else:
-            cv2.imshow('Depth', overlay)
+            # cv2.imshow('Depth', overlay)
+            cv2.imshow('Depth', image)
 
         last_image = image
 
@@ -142,7 +144,10 @@ def init_mqtt():
     global client
     client = mqtt.Client()
     client.on_connect = on_connect
+    # At home
     client.connect('192.168.1.105', 1883, 60)
+    # At work
+    # client.connect('192.168.5.184', 1883, 60)
     client.loop_start()
 
 cv2.namedWindow('Depth')
